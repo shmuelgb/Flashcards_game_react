f0621ae2a02a7a271f7e7e23ad25395b25734f4d
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2021-12-25 23:18:37 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,23 @@ import "./App.css";
 
 export default class App extends Component {
   state = {
-    deck: data,
+    deck: null,
     shownCards: [],
     completed: 0,
     progression: null,
+    answers: 0,
+    win: null,
   };
 
   componentDidMount = () => {
-    this.setState({ progression: this.state.deck.length });
-    console.log(this.state.deck);
+    this.getData();
+  };
+
+  getData = () => {
+    this.setState({ deck: data }, () => {
+      this.setState({ progression: this.state.deck.length });
+      this.shuffle();
+    });
   };
 
   shuffle = () => {
@@ -23,16 +31,53 @@ export default class App extends Component {
       const index = Math.floor(Math.random() * deck.length);
       if (!newDeck.includes(deck[index])) {
         newDeck.push(deck[index]);
-        console.log(index);
       }
     }
-    console.log("newDeck", newDeck);
+    console.log("newDeck:", newDeck);
+  };
+
+  handleDelete = (id) => {
+    const updatedDeck = this.state.deck.filter((card) => card.id !== id);
+    this.setState({ deck: updatedDeck });
+  };
+
+  handleNext = () => {
+    this.setState({ progression: this.state.progression - 1 });
+    this.checkGameProgression();
+  };
+
+  handleCheckAnswer = (answer) => {
+    if (answer) {
+      this.setState({ answers: this.state.answers + 1 });
+    }
+  };
+
+  checkGameProgression = () => {
+    if (this.state.progression >= 0 && this.state.answers < 3) {
+      return true;
+    } else if (this.state.progression < 0) {
+      this.setState({ win: "lose" });
+      return false;
+    } else if (this.state.answers >= 3) {
+      this.setState({ win: "win" });
+      return false;
+    }
   };
 
   render() {
     return (
       <div>
         <button onClick={this.shuffle}>shuffle</button>
+        {this.state.deck && (
+          <Card
+            handleNext={this.handleNext}
+            handleCheckAnswer={this.handleCheckAnswer}
+            card={this.state.deck[this.state.progression - 1]}
+            answers={this.state.answers}
+          />
+        )}
+        {this.state.win === "lose" && <h1>You lost :(</h1>}
+        {this.state.win === "win" && <h1>You won! :)</h1>}
       </div>
     );
   }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,57 @@
 import React, { Component } from "react";
 
 export default class Card extends Component {
+  state = { type: "q", card: this.props.card };
+
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.card !== this.props.card) {
+      this.setState({ card: this.props.card });
+    }
+  };
+
+  renderContent = () => {
+    if (this.state.card) {
+      if (this.state.type === "q") {
+        return <div>{this.state.card.q}</div>;
+      } else {
+        return <div>{this.state.card.a}</div>;
+      }
+    }
+  };
+
+  handleAnswer = () => {
+    this.setState({ type: "a" });
+  };
+
+  handleCheckAnswer = (e, answer) => {
+    this.props.handleCheckAnswer(answer);
+    this.handleNext();
+  };
+
+  handleNext = () => {
+    this.props.handleNext();
+    this.setState({ type: "q" });
+  };
+
+  renderCheckAnswer = () => {
+    return (
+      <div>
+        <p>Did u get it right?</p>
+        <button onClick={(e) => this.handleCheckAnswer(e, true)}>Yes</button>
+        <button onClick={this.handleCheckAnswer}>No</button>
+      </div>
+    );
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {this.renderContent()}
+        <button onClick={this.handleNext}>New Card</button>
+        <button onClick={this.handleAnswer}>Reveal Answer</button>
+        {this.state.type === "a" && this.renderCheckAnswer()}
+        Completed: {this.props.answers}/4
+      </div>
+    );
   }
 }
